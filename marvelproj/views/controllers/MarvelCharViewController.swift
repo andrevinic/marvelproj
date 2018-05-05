8a482c301a279452556ebd009c44616c9dd3cdd2
Andre Nogueira <avinogueira@yahoo.com>
2018-05-05 12:20:19 -0300

    collectionview

--- marvelproj/views/controllers/MarvelCharViewController.swift
+++ marvelproj/views/controllers/MarvelCharViewController.swift
@@ -11,13 +11,14 @@ import UIKit
 class MarvelCharViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var collectionView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var favoriteCollectionView: UICollectionView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var characters: NSMutableArray!
     var offset : Int = 0
     var limit: Int = 10
+    var showDataWithList = true
 }
 
 extension MarvelCharViewController{
@@ -26,6 +27,7 @@ extension MarvelCharViewController{
         super.viewDidLoad()
         self.characters = NSMutableArray()
         self.setupTableView()
+        self.setupCollectionView()
         self.activityIndicator.startAnimating()
         self.fetchCharacters()
         
@@ -39,25 +41,41 @@ extension MarvelCharViewController{
             self.tableView.reloadData()
             self.activityIndicator.stopAnimating()
             self.offset += self.limit
-            self.tableView.finishInfiniteScroll()
-            self.tableView.isHidden = false
+//            self.tableView.finishInfiniteScroll()
+            if(self.showDataWithList){
+                self.setupTableView()
+            }else{
+                self.setupCollectionView()
+            }
         }
     }
     func setupTableView(){
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        self.tableView.isHidden = true
+        self.tableView.isHidden = false
+        self.collectionView.isHidden = true
         self.tableView.register(UINib(nibName:"CharacterTableViewCell", bundle: nil), forCellReuseIdentifier: "CharacterTableViewCell")
+        self.tableView.reloadData()
+        self.showDataWithList = true
+        self.tableView.finishInfiniteScroll()
     }
+    
+    func setupCollectionView(){
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.isHidden = false
+        self.tableView.isHidden = true
+        self.showDataWithList = false
+        self.collectionView.register(UINib(nibName:"CharacterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CharacterCollectionViewCell")
+        self.collectionView.reloadData()
+        self.collectionView.finishInfiniteScroll()
+    }
+
 }
 
 extension MarvelCharViewController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if(characters.count == 0){
-            return 0
-        }
-        
         return characters.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
@@ -65,8 +83,8 @@ extension MarvelCharViewController: UITableViewDelegate, UITableViewDataSource{
             return UITableViewCell()
         }
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "CharacterTableViewCell", for: indexPath) as! CharacterTableViewCell
-        let character = self.characters.object(at: indexPath.row)
-        cell.setupCell(char: character as! Character)
+        let character = self.characters.object(at: indexPath.row) as! Character
+        cell.setupCell(char: character)
         
         return cell
     }
@@ -78,6 +96,52 @@ extension MarvelCharViewController: UITableViewDelegate, UITableViewDataSource{
                 self.fetchCharacters()
             })
         }
+    }
+}
+
+extension MarvelCharViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if(characters.count == 0){
+            return UICollectionViewCell()
+        }
+        
+        let cell = self.collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCollectionViewCell", for: indexPath) as! CharacterCollectionViewCell
+        let character = self.characters.object(at: indexPath.row) as! Character
+        cell.setupCell(char: character)
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.characters.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        
+        if(indexPath.row == (self.characters.count) - 1){
+            self.collectionView.addInfiniteScroll(handler: { (collectionView) in
+                self.fetchCharacters()
+            })
+        }
+    }
+}
+
+extension MarvelCharViewController:UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let collectionViewSize = collectionView.frame.size.width
+        return CGSize(width: collectionViewSize, height: collectionViewSize)
+    }
+}
+
+extension MarvelCharViewController{
+    
+    @IBAction func pressedGrid(_ sender: UIButton){
+        self.setupCollectionView()
+    }
+    
+    @IBAction func pressedList(_ sender: UIButton){
+        self.setupTableView()
     }
 }
 

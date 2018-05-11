b14d58d8856a8a2e852a46922da08a398d99bfbd
Andre Nogueira <avinogueira@yahoo.com>
2018-05-11 20:02:05 -0300

    Building image cache..avoid flickering

--- marvelproj/extensions/UIImageView+FetchImage.swift
+++ marvelproj/extensions/UIImageView+FetchImage.swift
@@ -7,7 +7,23 @@
 //
 
 import UIKit
+class ImageCache{
+    let imageCache = NSCache<NSString, UIImage>()
+    static let shared : ImageCache = ImageCache()
+
+    func image(url: URL, completion: @escaping (UIImage?) -> Void ){
+        
+        if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
+            completion(cachedImage)
+        }
+    }
+    init() {
+        
+    }
+}
+
 extension UIImageView {
+    
     func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleToFill) {
         contentMode = mode
         URLSession.shared.dataTask(with: url) { data, response, error in
@@ -25,6 +41,22 @@ extension UIImageView {
     func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleToFill) {
         guard let url = URL(string: link) else { return }
         downloadedFrom(url: url, contentMode: mode)
+    }
+    
+    func setImage(from url: URL, placeholder: UIImage? = nil) {
+        image = placeholder               // use placeholder (or if `nil`, remove any old image, before initiating asynchronous retrieval
+        ImageCache.shared.image(url: url) { (image) in
+            self.image = image
+        }
+//        ImageCache.shared.image(url: url) { [weak self] result in
+//            switch result {
+//            case .success(let image):
+//                self?.image = image
+//
+//            case .failure:
+//                break
+//            }
+//        }
     }
 }
 
